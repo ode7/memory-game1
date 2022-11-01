106f52e86e7bb8936f338e064f3e5d8b9a8a7c2d
ode7 <112641167+ode7@users.noreply.github.com>
2022-11-02 05:28:48 +0545

    1 file modified

--- script.js
+++ script.js
@@ -48,16 +48,16 @@ const cardArray = [
     img: 'images/hotdog.png'
   }
 ]
-
+const grid = document.querySelector('.grid')
 cardArray.sort(() => 0.5 - Math.random())
 
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement('img')
-    card.setAttribute('src', 'images/blank.png')
+    card.setAttribute('src', 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/blank.png')
     card.setAttribute('data-id', i)
-    card.addEventListener('click', flipCard)
-    grid.appendChild(card)
+    grid.append(card)
   }
 }
+createBoard()
