f937205db2e979804820b9ff4f2b3ea83a394bbf
ode7 <112641167+ode7@users.noreply.github.com>
2022-11-02 05:49:37 +0545

    3 files modified

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
 const cardArray = [
   {
     name: 'fries',
-    img: 'images/fries.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/fries.png'
   },
   {
     name: 'cheeseburger',
-    img: 'images/cheeseburger.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/cheeseburger.png'
   },
   {
     name: 'ice-cream',
-    img: 'images/ice-cream.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/ice-cream.png'
   },
   {
     name: 'pizza',
-    img: 'images/pizza.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/pizza.png'
   },
   {
     name: 'milkshake',
-    img: 'images/milkshake.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/milkshake.png'
   },
   {
     name: 'hotdog',
-    img: 'images/hotdog.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/hotdog.png'
   },
   {
     name: 'fries',
-    img: 'images/fries.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/fries.png'
   },
   {
     name: 'cheeseburger',
-    img: 'images/cheeseburger.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/cheeseburger.png'
   },
   {
     name: 'ice-cream',
-    img: 'images/ice-cream.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/ice-cream.png'
   },
   {
     name: 'pizza',
-    img: 'images/pizza.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/pizza.png'
   },
   {
     name: 'milkshake',
-    img: 'images/milkshake.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/milkshake.png'
   },
   {
     name: 'hotdog',
-    img: 'images/hotdog.png'
+    img: 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/hotdog.png'
   }
 ]
 const grid = document.querySelector('.grid')
 cardArray.sort(() => 0.5 - Math.random())
-
+const cardChosen =[]
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement('img')
     card.setAttribute('src', 'https://stackblitz.com/files/web-platform-emme2g/github/ode7/memory-game1/main/image/blank.png')
     card.setAttribute('data-id', i)
+    card.addEventListener('click',flipcard)
     grid.append(card)
   }
 }
 createBoard()
+function flipcard(){
+  const cardId = this.getAtttribute('data-id')
+  cardChosen.push(cardArray[cardId].name)
+  this.setAttribute('src',cardArray[cardId].img)
+}
